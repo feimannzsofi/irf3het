7b8c97489153509bcbf9667b88daecb71b5837bb
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-01 12:18:54 +0200

    Add write to file function

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,7 +41,7 @@
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(67, 49);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.Size = new System.Drawing.Size(239, 212);
             this.listBox1.TabIndex = 0;
             // 
             // button1
@@ -56,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(304, 71);
+            this.label1.Location = new System.Drawing.Point(372, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 2;
@@ -64,16 +65,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(449, 65);
+            this.textBox1.Location = new System.Drawing.Point(475, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -91,6 +103,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,10 @@ namespace UserMaintenance
         {
             InitializeComponent();
             InitializeComponent();
-            label1.Text = Resource1.FullName; // label1
+            label1.Text = Resource1.FullName; 
             
-            button1.Text = Resource1.Add; // button1
+            button1.Text = Resource1.Add; 
+            button2.Text = Resource1.WriteToFile;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -36,6 +38,26 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                
+                    foreach (var u in users)
+                    
+                    sw.WriteLine(string.Format(
+                        "{0};{1}",
+                        u.ID,
+                        u.FullName));
+                    
+                
+            }
         }
     }
 }
