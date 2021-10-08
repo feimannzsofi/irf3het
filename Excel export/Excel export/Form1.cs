658ffacd1354f18962b072ec3ca5e321107c5ac4
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-08 18:06:07 +0200

    CreateExcel

--- Excel export/Excel export/Form1.cs
+++ Excel export/Excel export/Form1.cs
@@ -22,7 +22,7 @@ namespace Excel_export
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
 
-        
+
 
         public Form1()
         {
@@ -38,7 +38,37 @@ namespace Excel_export
 
         public void CreateExcel()
         {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
 
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
+
+
     }
 }
