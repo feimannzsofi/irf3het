839c9297ae3235ab8a7bdb09ee286a0b83554507
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-08 19:54:13 +0200

    FormatTable

--- Excel export/Excel export/Form1.cs
+++ Excel export/Excel export/Form1.cs
@@ -14,7 +14,7 @@ namespace Excel_export
 {
     public partial class Form1 : Form
     {
-        private int _millian = (int)Math.Pow(10, 6);
+        private int _million = (int)Math.Pow(10, 6);
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
@@ -23,7 +23,7 @@ namespace Excel_export
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
 
-
+        string[] headers;
 
         public Form1()
         {
@@ -54,6 +54,7 @@ namespace Excel_export
                 xlSheet = xlWB.ActiveSheet;
 
                 CreateTable();
+                FormatTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -75,52 +76,56 @@ namespace Excel_export
 
         private void CreateTable()
         {
-
-            string[] headers = new string[] 
-            {
-                     "Kód",
-                     "Eladó",
-                     "Oldal",
-                     "Kerület",
-                     "Lift",
-                     "Szobák száma",
-                     "Alapterület (m2)",
-                     "Ár (mFt)",
-                     "Négyzetméter ár (Ft/m2)"
-            };
+            headers = new string[]
+           {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"
+           };
 
             for (int i = 0; i < headers.Length; i++)
-            {
-                xlSheet.Cells[1, i = 1] = headers[i];
-            }
+                xlSheet.Cells[1, i + 1] = headers[i];
 
             object[,] values = new object[lakasok.Count, headers.Length];
 
             int counter = 0;
-            int floorColumn = 6;
-            foreach (Flat f in lakasok)
+            int floorColum = 6;
+            foreach (var lakas in lakasok)
             {
-                values[counter, 0] = f.Code;
-                values[counter, 1] = f.Vendor;
-                values[counter, 2] = f.Side;
-                values[counter, 3] = f.District;
-
-                if (f.Elevator == true) {values[counter, 4] = true;}
-                else{values[counter, 4] = false;}
-
-                values[counter, 5] = f.NumberOfRooms;
-                values[counter, floorColumn] = f.FloorArea;
-                values[counter, 7] = f.Price;
-                values[counter, 8] = "=" + GetCell(counter + 2, 8) + _millian + GetCell(counter + 2, 7);
+                values[counter, 0] = lakas.Code;
+                values[counter, 1] = lakas.Vendor;
+                values[counter, 2] = lakas.Side;
+                values[counter, 3] = lakas.District;
+                values[counter, 4] = lakas.Elevator ?
+                    "Van"
+                    : "Nincs";
+                //if (lakas.Elevator)
+                //    values[counter, 4] = "Van";
+                //else
+                //    values[counter, 4] = "Nincs";
+                values[counter, 5] = lakas.NumberOfRooms;
+                values[counter, 6] = lakas.FloorArea;
+                values[counter, 7] = lakas.Price;                           //H2/G2*1000e
+                values[counter, 8] = string.Format("={0}/{1}*{2}",          //= jel
+                    "H" + (counter + 2).ToString(),
+                    GetCell(counter + 2, floorColum + 1),
+                    _million.ToString());
                 counter++;
             }
 
             var range = xlSheet.get_Range(
-                GetCell(2,1),
-                GetCell(1 + values.GetLength(0), values.GetLength(1)));
-            range.Value2 = values;
+                             GetCell(2, 1),
+                             GetCell(1 + values.GetLength(0), values.GetLength(1)));
 
+            range.Value2 = values;       
         }
+
         private string GetCell(int x, int y)
         {
             string ExcelCoordinate = "";
@@ -137,5 +142,27 @@ namespace Excel_export
 
             return ExcelCoordinate;
         }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range complateTableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, headers.Length));
+            //indulási pont     // utolso  sorszám
+            complateTableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
     }
 }
+
+
