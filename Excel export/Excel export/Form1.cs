6038b483622e422e41bbba75b182e612b46bafb6
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-08 17:50:39 +0200

    Data import

--- Excel export/Excel export/Form1.cs
+++ Excel export/Excel export/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Excel_export
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            List<Flat> Flats = context.Flats.ToList();
         }
     }
 }
